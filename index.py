d19f96378ec625fc22e6c8a6ca20c363b86443d1
jjnxed <115165777+jjnxed@users.noreply.github.com>
2022-12-18 17:23:10 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,10 @@
+import random
+print("Hello World")
+print (random.randrange(1, 20))
+
+x="Hello World!"
+print(x[-5:]) #Slicing string
+
+a="Hello"
+b="World"
+print(a+" "+b) #String  Concentation alas  Combining two strings together. The " " functions as adding space
