abfaaf62973b8b97a613b09ea119f945fa41f4bb
jjnxed <115165777+jjnxed@users.noreply.github.com>
2022-12-31 19:40:49 +0700

    Update index.py

--- index.py
+++ index.py
@@ -8,3 +8,10 @@ print(x[-5:]) #Slicing string
 a="Hello"
 b="World"
 print(a+" "+b) #String  Concentation alas  Combining two strings together. The " " functions as adding space
+
+
+
+birth_year= input("Enter your birth year: ") #converting data type
+age= 2023 - int(birth_year)
+print(age)
+
